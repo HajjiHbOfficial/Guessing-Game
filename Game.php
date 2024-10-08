0752d2ef633cbc79d3eb72b8bf60642e2250b839
Hajji <91248271+HajjiHbOfficial@users.noreply.github.com>
2024-10-08 14:22:32 +0200

    Create Game.php

---
+++ Game.php
@@ -0,0 +1,7 @@
+Welcome to my guessing game
+$correctnumber)
+{ echo "Your guess is too high"; }
+else if ($_GET['guess']==$correctnumber)
+{ echo "Congratulations - You are right"; }
+} else { echo "Missing guess parameter
+Your guess is too short"; } ?>
